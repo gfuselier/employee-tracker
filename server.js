f51df66538c6b4e2bfe839b06fd9cdefcd1917cf
gfuselier <gisellefuselier@gmail.com>
2023-10-23 09:01:29 -0500

    req'd mysql

---
+++ server.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost', //127.0.0.1
+      user: 'root',
+      password: 'roundTwo',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
