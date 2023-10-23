bafcf9de325013b6bfd7dde02b63cc2d12bd48e2
gfuselier <gisellefuselier@gmail.com>
2023-10-23 09:18:59 -0500

    created tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,25 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
+
+USE company_db;
+SELECT DATABASE();
+
+CREATE TABLE department (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
+);
