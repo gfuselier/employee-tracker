c011fa4ca3e058fc29956f5a75c99efce114ab12
gfuselier <gisellefuselier@gmail.com>
2023-10-23 22:09:04 -0500

    added foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,9 @@ CREATE TABLE roles (
   id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments(id) 
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
@@ -21,5 +23,9 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id) 
+  ON DELETE SET NULL
+  FOREIGN KEY (manager_id) REFERENCES employees(id) 
+  ON DELETE SET NULL
 );
