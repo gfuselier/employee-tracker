e54b0d262388d7353780f55e2d21be1f3af75ce1
gfuselier <gisellefuselier@gmail.com>
2023-10-23 22:53:50 -0500

    sample function

--- server.js
+++ server.js
@@ -22,15 +22,25 @@ const questions = [
 
   //prompt first, then have query inside. will be wet at first, don't worry
   inquirer.prompt(questions)
-    .then((data) => {
-        console.log(data);
-        if(data.action === 'View All Departments') {
-            db.query('SELECT * FROM departments', function (err, results) {
+    .then((answers) => {
+        console.log(answers);
+        if(answers.action === 'View All Departments') {
+            db.query('SELECT * FROM departments', (err, results) => {
                 if(err) {
-                    console.log(err)
-                  } console.table(results);
-              });
+                  console.log(err)
+                } console.table(results);
+            });
         }
     })
 
 
+// promptAction =()=> {
+//     return inquirer.prompt(questions)
+//     .then((answers) => {
+//         if (answers.action === "Quit") {
+//         return;
+//         } else {
+//         return promptAction();
+//         }
+//     })
+// };
